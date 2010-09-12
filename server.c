1d6fafd7cb6c57db5973c4de2639b704b6fdf6e1
Evan Travers <widgetwebworks@gmail.com>
2010-09-11 22:40:38 -0500

    Is able to read that there is incoming traffic, time to handle what comes next.

--- server.c
+++ server.c
@@ -22,29 +22,48 @@
 
 main(int argc, char *argv[]) {
 	struct sockaddr_in Server_Address_Passive;
-	int server_socket, socket_to_client;
+	int passive_socket;
 	int procid, tmp;
 	int open_sockets[16];
 	
 	char buf[512];
 	int msglength;
 	
-	// start up the passive socket
+	fd_set readfds;
+	int bytes, i;
+	
+	// set up the passive socket
+	passive_socket=Socket(AF_INET,SOCK_STREAM,0);
+	Server_Address_Passive.sin_family=AF_INET;
+	Server_Address_Passive.sin_addr.s_addr=htonl(INADDR_ANY);
+	Server_Address_Passive.sin_port=htons(atoi(argv[1]));
+	
+	int e_bin = Bind(passive_socket, (struct sockaddr *) &Server_Address_Passive, sizeof(struct sockaddr));
 	
 	// start listening
+	int e_lis = Listen(passive_socket,5);
 	
-	// when the passive socket gets a buzz
-	
-	// bind that connection to another socket
-	
-	// put that socket in the array
-	
-	// if data comes from that socket
-	
-	// read and print it back
-	
-	// if the data is quit then close that socket/connection
-	
+	while(1) {
+		FD_ZERO(&readfds);
+		FD_SET(0, &readfds);
+		FD_SET(passive_socket, &readfds);
+
+		// start up the select
+		i=select(16, &readfds, 0, 0, 0);
+
+		if (FD_ISSET(passive_socket, &readfds)) {
+			// when the passive socket gets a buzz
+			
+			// bind that connection to another socket
+
+			// put that socket in the array
+		}
+
+		// for each descriptor in the array
+		// check that for data
+		// if there is data, read it and write it back
+		
+		// if that data is quit, then close the connection cleanly
 /*
 	server_socket=Socket(AF_INET,SOCK_STREAM,0);
 	Server_Address_Passive.sin_family=AF_INET;
@@ -71,6 +90,6 @@ main(int argc, char *argv[]) {
 		
 		int e_clo = Close(socket_to_client);
 */
-		exit(0);
 	}
+	exit(0);
 }

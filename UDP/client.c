36b893c0c8820ff64d705d8ad547314c51960a54
Evan Travers <widgetwebworks@gmail.com>
2010-10-18 16:56:11 -0500

    baseline for submission

--- UDP/client.c
+++ UDP/client.c
@@ -26,17 +26,19 @@
 
 main(int argc, char *argv[]) {
 	if (argc != 4) {
-		printf("Usage: ./client <subnet mask (i.e. 255.255.255.255)> <port> <message> \n");
+		printf("Usage: ./client <subnet mask (i.e. 255.255.255.0)> <port> <message> \n");
 		exit(0);
 	}
 	
 	// initialize
 	int my_socket, on=1, off=0;
 	struct hostent *hp;
-	struct sockaddr_in client, server;
+	struct sockaddr_in server;
 	char buf[512];
 	int bytes;
 	unsigned int fromlen;
+	struct sockaddr_in listofservers[30];
+	int server_count=0, more_servers=1;
 
 	// initialize sockets	
 	hp = gethostbyname(argv[1]);
@@ -52,14 +54,28 @@ main(int argc, char *argv[]) {
 	server.sin_addr = *((struct in_addr *)hp->h_addr);
 	server.sin_addr.s_addr|=htonl(0xff);
 	
-	// send the message
+	// send the greeting
 	bytes = Sendto(my_socket, argv[3], strlen(argv[3]), 0, (struct sockaddr *)&server, sizeof(server));	
 	printf("sent %d bytes to %s\n", bytes, inet_ntoa(server.sin_addr));
-	Setsockopt(my_socket, SOL_SOCKET, SO_BROADCAST, &on, sizeof(off));	
 	
-	fromlen = sizeof(server);
-	bytes = Recvfrom(my_socket, buf, 512, 0, (struct sockaddr *) &server, &fromlen);
-	printf("Read from server: %s\n", buf);
+	// TODO detect multiple servers, add them to array
+	while(more_servers==1) {
+		
+		fromlen = sizeof(server);
+		bytes = Recvfrom(my_socket, buf, 512, 0, (struct sockaddr *) &server, &fromlen);
+		printf("Client: Found host at IP\t%s\nServer: %s\n", inet_ntoa(server.sin_addr), buf);
+		// add to the array
+		int i;
+		for (i=0; i < server_count; ++i) {
+			if (server.sin_addr.s_addr==listofservers[i].sin_addr.s_addr) {
+				break;
+			}
+		}
+		listofservers[server_count++]=server;
+	}	
+	
+	// turn off broadcast
+	Setsockopt(my_socket, SOL_SOCKET, SO_BROADCAST, &on, sizeof(off));
 	
 	Close(my_socket);
 }
--- UDP/server.c
+++ UDP/server.c
@@ -21,34 +21,36 @@
 #include "Close.h"
 #include "Sendto.h"
 
+// basic UDP client, returns everything it is sent until is sent a 'quit' message
 main(int argc, char *argv[]) {
 	if (argc != 2) {
-		printf("Usage: ./server <port> \n");
+		printf("Usage: ./client <port> \n");
 		exit(0);
 	}
 	
-	struct sockaddr_in from, server;
+	struct sockaddr_in client;
 	struct hostent *hp;
 	int my_socket, bytes;
 	char buf[512];
 	unsigned int fromlen;
 
 	my_socket=Socket(AF_INET, SOCK_DGRAM,0);
-	server.sin_family=AF_INET;
-	server.sin_addr.s_addr=htonl(INADDR_ANY);
-	server.sin_port=htons(atoi(argv[1]));
+	client.sin_family=AF_INET;
+	client.sin_addr.s_addr=htonl(INADDR_ANY);
+	client.sin_port=htons(atoi(argv[1]));
 	
-	Bind(my_socket, (struct sockaddr *) &server, sizeof(server));
+	Bind(my_socket, (struct sockaddr *) &client, sizeof(client));
 	
 	while(1) {
-		fromlen=sizeof(from);
-		bytes=Recvfrom(my_socket, buf, 512, 0, (struct sockaddr *) &from, &fromlen);
-		printf("SERVER: read %d bytes from IP %d (%s)\n", bytes, inet_ntoa(from.sin_addr), buf);
+		bzero(buf, sizeof(buf));
+		fromlen=sizeof(client);
+		bytes=Recvfrom(my_socket, buf, 512, 0, (struct sockaddr *) &client, &fromlen);
+		printf("SERVER: read %d bytes from client: (%s)\n", bytes, buf);
 		printf("Sending the data back...\n");
-		bytes=Sendto(my_socket, buf, strlen(buf)+1, 0, (struct sockaddr *) &from, sizeof(from));
+		bytes=Sendto(my_socket, buf, strlen(buf), 0, (struct sockaddr *) &client, sizeof(client));
 		if (!strcmp(buf, "quit")) {
-			// Should send quit to the client
-			break;
+			printf("Quitting!\n");
+			exit(0);
 		}
 	}
 	Close(my_socket);

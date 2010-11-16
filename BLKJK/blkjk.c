c3300d7fcfb92281f53d3d79d0cf9e2d88f2698b
Evan Travers <widgetwebworks@gmail.com>
2010-11-16 15:27:03 -0600

    Hit works!

--- BLKJK/blkjk.c
+++ BLKJK/blkjk.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-// TODO implement the twist random number dev
-#include "mtwist.h"
 
 int getCard();
 int stnd(int, int, int, int, int);
@@ -40,22 +38,15 @@ int main(int argc, char *argv[]) {
 	printf("Generating %d hands using your hand: %d, %d, (%d), and dealer up: %d...\n", iterations, atoi(argv[1]), atoi(argv[2]), optHand(yourHand, aceFlagY), optHand(dealerHand, aceFlagD));
 
 	// time to compute
+	printf("Calculating stand...\n");
 	float stand = (float)stnd(yourHand, dealerHand, iterations, aceFlagY, aceFlagD)/iterations;
+	printf("Calculating double...\n");
 	float dbl = (float)dubl(yourHand, dealerHand, iterations, aceFlagY, aceFlagD)/iterations;
+	printf("Calculating hit...\n");
 	float hit = (float)hitd(yourHand, dealerHand, iterations, aceFlagY, aceFlagD)/iterations;
-
 	printf("Stand odds:\t%f\n", stand);
 	printf("Double odds:\t%f\n", dbl);
 	printf("Hit odds:\t%f\n", hit);
-	if (stand>dbl&&dbl>hit) {
-		printf("You should stand.\n");
-	}
-	if (dbl>stand&&dbl>hit) {
-		printf("You should double.\n");
-	}
-	if (hit>stand&&hit>dbl) {
-		printf("You should hit.\n");
-	}
 	
 	// TODO return the one that has the best winning ratio
 }
@@ -84,7 +75,7 @@ int stnd(int yourHand, int dealerHand, int iterations, int aceFlagY, int aceFlag
 		// you sit here
 		
 		// dealer hits until 17
-		while (optHand(dHand,aFlagD)<18) {
+		while (optHand(dHand,aFlagD)<17) {
 			int newCard=getCard();
 			if (newCard==1) {
 				aFlagD=1;
@@ -131,7 +122,7 @@ int dubl(int yourHand, int dealerHand, int iterations, int aceFlagY, int aceFlag
 		// you sit here
 		
 		// dealer hits until 17
-		while (optHand(dHand,aFlagD)<18) {
+		while (optHand(dHand,aFlagD)<17) {
 			int newCard=getCard();
 			if (newCard==1) {
 				aFlagD=1;
@@ -165,63 +156,43 @@ int hitd(int yourHand, int dealerHand, int iterations, int aceFlagY, int aceFlag
 		dHand = dealerHand;
 		
 		// are either you or the dealer holding an ace?
-		aFlagD=aceFlagD;
-		aFlagY=aceFlagY;
+		aFlagD = aceFlagD;
+		aFlagY = aceFlagY;
 		
-		// your turn
-		// you hit once
-		int newCard = getCard();
-		if (newCard==1) {
-			aFlagY=1;
-		}
-		yHand=yHand+newCard;
-		// you sit here
-		if (optHand(yHand,aFlagY)>22) {
-			odds--;
-			break;
-		}
-		
-		// dealer hits
-		if (optHand(dHand,aFlagD)<18) {
-			int newCard=getCard();
+		while (1) {
+			// you hit once
+			int newCard = getCard();
 			if (newCard==1) {
-				aFlagD=1;
+				aFlagY=1;
 			}
-			dHand=dHand+newCard;
-		}
-		
-		if (optHand(dHand,aFlagD)>22) {
-			odds++;
-			break;
-		}
-		
-		// TODO consider how to handle the hand changing constantly. Static?
-		// ALSO: there's an issue with static being run twice here. Not exactly the same params.
-		// try all the options for this hand.	
-		
-		// stand
-		int stndodds = stnd(yHand, dHand, iterations, aFlagY, aFlagD);
-		// hit
-		int hitdodds = hitd(yHand, dHand, iterations, aFlagY, aFlagD);
-		
-		if (stndodds>hitdodds) {
-			return stndodds;
-		}
-		else {
-			return hitdodds;
-		}
-		
-		// final positions
-		int dHandF = optHand(dHand,aFlagD);
-		int yHandF = optHand(yHand,aFlagY);
-		
-		// if your hands are not the same (tie)
-		if (dHandF!=yHandF) {
-			if ((yHandF>dHandF)&&(yHandF<22)||(yHandF<22)&&(dHandF>21)) {
-				odds=odds+1;
+			yHand=yHand+newCard;
+
+			if (optHand(yHand, aFlagY)>21) {
+				odds--;
+				break;
 			}
-			else {
-				odds=odds-1;
+			
+			// dealer hits until 17
+			while (optHand(dHand, aFlagD)<17) {
+				int newCard=getCard();
+				if (newCard==1) {
+					aFlagD=1;
+				}
+				dHand=dHand+newCard;
+			}
+			
+			int dHandF = optHand(dHand,aFlagD);
+			int yHandF = optHand(yHand,aFlagY);
+			
+			if (dHandF!=yHandF) {
+				if ((yHandF>dHandF)&&(yHandF<22)||(yHandF<22)&&(dHandF>21)) {
+					odds++;
+					break;
+				}
+				else {
+					odds--;
+					break;
+				}
 			}
 		}
 		cnt++;
